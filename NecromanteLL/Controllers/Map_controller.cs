82d490cec1615312ab9217689451deca41960a65
Daniel Sousa duplat <ddaniel1912@gmail.com>
2018-03-14 10:19:58 -0300

    sem mudanças

---
+++ NecromanteLL/Controllers/Map_controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NecromanteLL.Controllers {
+    class Map_controller {
+        private List<Itens> itens;
+        internal List<Itens> Itens { get => Itens; set => Itens = value; }
+
+        Itens = new List<Itens>();
+
+        Escudo a = new Escudo("Escudo de madeira", 0, 20, 10, 0);
+    }
+}
