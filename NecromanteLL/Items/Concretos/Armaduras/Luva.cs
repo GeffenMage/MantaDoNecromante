0b14710fb1fecb286327497006daf835a8d08dab
DigitalWord <paulo.miranda107@gmail.com>
2018-03-21 10:27:12 -0300

    Correção imagens

--- NecromanteLL/Items/Concretos/Armaduras/Luva.cs
+++ NecromanteLL/Items/Concretos/Armaduras/Luva.cs
@@ -7,14 +7,14 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace NecromanteLL {
     public class Luva : Armaduras {
-        public Luva(String nome, int dmg, int def, int hp_up, int mp_up) {
+        public Luva(String nome, int dmg, int def, int hp_up, int mp_up, String figura) {
             this.Nome = nome;
             this.Dmg = dmg;
             this.Def = def;
             this.Hp_up = hp_up;
             this.Mp_up = mp_up;
 
-            this.Sprite = new BitmapImage(new Uri("ms-appx:///GameAssets/Characters/itens/glove_t.png"));
+            this.Sprite.Source = new BitmapImage(new Uri(figura));
         }
     }
 }
