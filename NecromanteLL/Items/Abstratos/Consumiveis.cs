880eb59d458530fc48634c9a57543ce47a04c821
DigitalWord <paulo.miranda107@gmail.com>
2018-04-13 11:24:16 -0300

    Adição de itens consumiveis

---
+++ NecromanteLL/Items/Abstratos/Consumiveis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace NecromanteLL {
+    public abstract class Consumiveis : Itens {
+        //implementar alguma mecânica unica para Consumiveis
+    }
+}
+
