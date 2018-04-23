b812e7c18952c58d6114be90835c718955b86875
Ewerton <ewerton1555@gmail.com>
2018-04-23 10:19:44 -0300

    adição de imagens

--- NecromanteLL/Controllers/Map_controller.cs
+++ NecromanteLL/Controllers/Map_controller.cs
@@ -50,7 +50,7 @@ namespace NecromanteLL {
             itens.Add(new Bota("Bota de Couro", 0, 20, 0, 10, "ms-appx:///GameAssets/Characters/itens/bota_1.png"));
             itens.Add(new Bota("Bota de Bronze", 10, 20, 10, 0, "ms-appx:///GameAssets/Characters/itens/bota_2.png"));
             itens.Add(new Calca("Calca de Couro", 0, 20, 0, 10, "ms-appx:///GameAssets/Characters/itens/pant.png"));
-            itens.Add(new Calca("Calca de Bronze", 10, 30, 10, 0, "ms-appx:///GameAssets/Characters/itens/pant.png"));
+            itens.Add(new Calca("Calca de Bronze", 10, 30, 10, 0, "ms-appx:///GameAssets/Characters/itens/v2/pant_t"));
             itens.Add(new Capacete("Capacete de Couro", 0, 20, 10, 0, "ms-appx:///GameAssets/Characters/itens/capacete_1.png"));
             itens.Add(new Capacete("Capacete de Bronze", 0, 50, 10, 0, "ms-appx:///GameAssets/Characters/itens/capacete_2.png"));
             itens.Add(new Cota("Cota de Couro", 10, 10, 0, 10, "ms-appx:///GameAssets/Characters/itens/v2/Armor 1.png"));
@@ -87,8 +87,11 @@ namespace NecromanteLL {
             mob_num = new Random();
             Mob[] vet = mobs.ToArray();
             vet[0].Sprite.Source = new BitmapImage(new Uri("ms-appx:///GameAssets/Characters/enemies/exqueleton/skeletonIdle.gif"));
+
             //Mobs_do_mapa[pos_x, pos_y] = vet[mob_num.Next(0,12)];
             Mobs_do_mapa[pos_x, pos_y] = vet[0];
+           
+
         }
 
         public Mob FindMob(int pos_x, int pos_y) {
