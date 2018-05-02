cdd2439e6c7c506db118f106603083ef0ab13949
DigitalWord <paulo.miranda107@gmail.com>
2018-05-02 11:00:10 -0300

    modificação nos inimigos

--- NecromanteLL/Enemys/Fantasma.cs
+++ NecromanteLL/Enemys/Fantasma.cs
@@ -17,7 +17,7 @@ namespace NecromanteLL {
                     Nome = "Espirito";
                     Hp_total = 1200; Hp_atual = Hp_total;
                     Mp_total = 600; Mp_atual = Mp_total;
-                    Given_xp = 1200; Lvl = 30;
+                    Given_xp = 120000; Lvl = 10;
                     Base_def = 60; Base_dmg = 200;
                     //Inicializa os sprites do inimigo
                     Sprite = new Image();
@@ -26,16 +26,16 @@ namespace NecromanteLL {
                     Sprite.Source = new BitmapImage(new Uri("ms-appx:///GameAssets/Characters/heroes/mage/MagoParadoLeft.gif"));
 
                     //Inicializa as skills do inimigo
-                    Skills.Add(new Skill("Atk Up I", 0, 20, 1, 0, 0, 40, 0, 0));
-                    Skills.Add(new Skill("Regen MP I", 60, 0, 1, 0, 180, 0, 0, 0));
-                    Skills.Add(new Skill("Drain Life I", 80, 0, 1, 100, 0, 0, 0, 100));
+                    Skills.Add(new Skill("Atk Up I", 0, 20, 10, 0, 0, 40, 0, 0));
+                    Skills.Add(new Skill("Regen MP I", 60, 0, 15, 0, 180, 0, 0, 0));
+                    Skills.Add(new Skill("Drain Life I", 80, 0, 20, 100, 0, 0, 0, 100));
 
                     break;
                 case 2:
                     Nome = "Espirito Vingativo";
                     Hp_total = 1500; Hp_atual = Hp_total;
                     Mp_total = 800; Mp_atual = Mp_total;
-                    Given_xp = 1500; Lvl = 40;
+                    Given_xp = 150000; Lvl = 15;
                     Base_def = 70; Base_dmg = 240;
                     //Inicializa os sprites do inimigo
                     Sprite = new Image();
@@ -44,16 +44,16 @@ namespace NecromanteLL {
                     Sprite.Source = new BitmapImage(new Uri("ms-appx:///GameAssets/Characters/heroes/mage/MagoParadoLeft.gif"));
 
                     //Inicializa as skills do inimigo
-                    Skills.Add(new Skill("Fall II", 0, 20, 1, 0, 0, 0, 0, 70));
-                    Skills.Add(new Skill("Regen Mp II", 80, 0, 1, 0, 240, 0, 0, 0));
-                    Skills.Add(new Skill("Drain Life II", 120, 0, 1, 250, 0, 0, 0, 250));
+                    Skills.Add(new Skill("Fall II", 0, 20, 10, 0, 0, 0, 0, 70));
+                    Skills.Add(new Skill("Regen Mp II", 80, 0, 15, 0, 240, 0, 0, 0));
+                    Skills.Add(new Skill("Drain Life II", 120, 0, 20, 250, 0, 0, 0, 250));
 
                     break;
                 case 3:
                     Nome = "Ceifador";
                     Hp_total = 2000; Hp_atual = Hp_total;
                     Mp_total = 1000; Mp_atual = Mp_total;
-                    Given_xp = 2000; Lvl = 60;
+                    Given_xp = 200000; Lvl = 20;
                     Base_def = 80; Base_dmg = 260;
                     //Inicializa os sprites do inimigo
                     Sprite = new Image();
@@ -63,9 +63,9 @@ namespace NecromanteLL {
                     Sprite.Source = new BitmapImage(new Uri("ms-appx:///GameAssets/Characters/heroes/mage/MagoParadoLeft.gif"));
 
                     //Inicializa as skills do inimigo
-                    Skills.Add(new Skill("Fall III", 0, 20, 1, 0, 0, 0, 0, 70));
-                    Skills.Add(new Skill("Regen Mp III", 120, 0, 1, 0, 360, 0, 0, 0));
-                    Skills.Add(new Skill("Drain Life III", 190, 0, 1, 500, 0, 0, 0, 500));
+                    Skills.Add(new Skill("Fall III", 0, 20, 10, 0, 0, 0, 0, 70));
+                    Skills.Add(new Skill("Regen Mp III", 120, 0, 15, 0, 360, 0, 0, 0));
+                    Skills.Add(new Skill("Drain Life III", 190, 0, 20, 500, 0, 0, 0, 500));
 
                     break;
             }
